498964f3853149041a715843d1dc9ffc7b7ea798
Castro32 <okanga.fidel@gmail.com>
2024-02-04 23:13:48 +0300

    added new navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href='/' className='flex items-center gap-1'>
+          <Image 
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt='logo'
+          />
+          <p className='nav-logo'></p>
+        </Link>
+      </nav>
+    </header>
   )
 }
 
